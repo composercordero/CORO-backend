37c2ddd1a96eb4763b57562cc47230d4cfc89c80
composercordero <composercordero@gmail.com>
2023-08-29 21:02:02 -0500

    initial commit, flask template

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+# from app import app, db
+# from flask import render_template, redirect, url_for, flash, request
+# from app.forms import IMPORT NAMES OF FORMS
+# from app.models import IMPORT NAMES FROM MODELS
+# from flask_login import login_user, logout_user, login_required, current_user
+
+# @app.route('/', methods = ['GET', 'POST'])
+# def index():
+#     return render_template('index.html')
+
+
+# @app.route('/logout', methods = ['GET', 'POST'])
+# def logout():
+#     logout_user()
+#     flash("You have successfully logged out", "success")
+#     return redirect(url_for('index'))
